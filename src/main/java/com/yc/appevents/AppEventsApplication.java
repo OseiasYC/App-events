4b870df8668c32c40e208016418f3e1593749033
Oseias Lopes <83646294+OseiasYC@users.noreply.github.com>
2022-09-20 15:49:24 -0300

    Added project

---
+++ src/main/java/com/yc/appevents/AppEventsApplication.java
@@ -0,0 +1,13 @@
+package com.yc.appevents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppEventsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppEventsApplication.class, args);
+	}
+
+}
